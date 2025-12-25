697068a872907f37081864425d3206c7cdbe0054
prasanth844 <135524731+prasanth844@users.noreply.github.com>
2025-12-25 16:23:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+// Calculator logic
+document.getElementById("calculate").addEventListener("click", function () {
+	let num1 = Number(document.getElementById("num1").value);
+	let num2 = Number(document.getElementById("num2").value);
+	let operator = document.getElementById("operator").value;
+	let result;
+
+	if (operator === "+") {
+		result = num1 + num2;
+	} else if (operator === "-") {
+		result = num1 - num2;
+	} else if (operator === "*") {
+		result = num1 * num2;
+	} else if (operator === "/") {
+		result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+	}
+
+	document.getElementById("result").innerText = result;
+});
+
+// Change text logic
+document.getElementById("change_text").addEventListener("click", function () {
+	let newText = ["Bye", "Good Bye", "To", "Your", "Class"];
+	let elements = document.getElementsByClassName("division");
+
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].innerText = newText[i];
+	}
+});
